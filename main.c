0ecd6c5798266f04b26477a8d625b2cdb348a636
Varshini-ram <169165137+Varshini-ram@users.noreply.github.com>
2024-05-07 19:08:03 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    if (age >= 18) {
+        printf("You are eligible to vote! 🗳️\n");
+    } else {
+        printf("Sorry, you are not eligible to vote yet.\n");
+    }
+
+    return 0;
+}
